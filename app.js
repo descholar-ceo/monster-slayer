117f60c9f35451c9d50cc5bd31c88b4582064cfb
descholar-ceo <emmanuellamugi@gmail.com>
2021-06-27 01:06:16 +0200

    Adjust the health bars to zero

--- app.js
+++ app.js
@@ -13,9 +13,15 @@ const app = Vue.createApp({
     },
     computed: {
         monsterBarStyles(){
+            if(this.monsterHealth < 0){
+                return {width: '0%'}
+            }
            return {width: this.monsterHealth + '%'}
         },
         playerBarStyles(){
+            if(this.playerHealth < 0){
+                return {width: '0%'}
+            }
            return {width: this.playerHealth + '%'}
         },
         enableSpecialAttack(){
